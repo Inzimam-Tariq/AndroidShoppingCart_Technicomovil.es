10693630e62f66b23e7412e1558bad69f752275e
Inzimam Tariq <inzi.programmer@gmail.com>
2018-03-01 11:36:50 +0500

    Added Complete Code From Android_Shopping_Cart for Further Processing

---
+++ app/src/main/java/com/web2design/souqclone/app/model/MenuSubCategory.java
@@ -0,0 +1,32 @@
+package com.web2design.souqclone.app.model;
+
+/**
+ * Created by Inzimam on 30-Oct-17.
+ */
+
+public class MenuSubCategory {
+
+    private String menuSubCategoryId;
+    private String menuSubCategoryName;
+
+    public MenuSubCategory(String menuSubCategoryId, String menuSubCategoryName) {
+        this.menuSubCategoryId = menuSubCategoryId;
+        this.menuSubCategoryName = menuSubCategoryName;
+    }
+
+    public String getMenuSubCategoryId() {
+        return menuSubCategoryId;
+    }
+
+    public void setMenuSubCategoryId(String menuSubCategoryId) {
+        this.menuSubCategoryId = menuSubCategoryId;
+    }
+
+    public String getMenuSubCategoryName() {
+        return menuSubCategoryName;
+    }
+
+    public void setMenuSubCategoryName(String menuSubCategoryName) {
+        this.menuSubCategoryName = menuSubCategoryName;
+    }
+}
